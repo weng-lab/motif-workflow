e51c9babf0bf74f8360179050f7204e98d3d50cb
hpratt <hpratt111@live.com>
2023-03-14 17:12:10 -0400

    bump Krews

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ group = "com.genomealmanac"
 version = "1.0.0"
 
 repositories {
+    mavenLocal()
     jcenter()
     maven {
       url = uri("https://maven.pkg.github.com/weng-lab/krews")
@@ -23,7 +24,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    compile("io.krews", "krews", "0.12.0")
+    compile("io.krews", "krews", "0.13.1")
     implementation("com.squareup.okhttp3", "okhttp", "3.12.1")
     implementation("com.squareup.moshi", "moshi-kotlin", "1.8.0")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.4.0")
